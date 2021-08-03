67ba61372e393f71ce61870a9c31b6a99364ce98
PEIYAN <45463777+PeiyanFlying@users.noreply.github.com>
2021-08-03 12:14:59 -0400

    Add files via upload

--- vit_l2.py
+++ vit_l2.py
@@ -34,9 +34,14 @@ from utils import batch_index_select
 from timm.data import IMAGENET_DEFAULT_MEAN, IMAGENET_DEFAULT_STD
 from timm.models.layers import DropPath, to_2tuple, trunc_normal_
 
+# coding=UTF-8
+import xlrd
+import xlwt
+from xlutils.copy import copy
+
 _logger = logging.getLogger(__name__)
 
-
+book_name_xls = 'score_recoder.xls'
 
 
 def _cfg(url='', **kwargs):
@@ -427,9 +432,6 @@ class VisionTransformerDiffPruning(nn.Module):
         x = x + self.pos_embed
         x = self.pos_drop(x)
 
-        # if not self.training:
-        #     file = open('sparsity.json','a+')
-
         p_count = 0
         out_pred_prob = []
         init_n = 14 * 14
@@ -457,7 +459,7 @@ class VisionTransformerDiffPruning(nn.Module):
                     prev_decision = hard_keep_decision
 
                     score = pred_score.cpu().numpy().tolist()
-                    # file.write(str(score)+ '\n')
+                    write_excel_xls_append(book_name_xls, score)
                 p_count += 1
             else:
                 if self.training:
@@ -477,7 +479,6 @@ class VisionTransformerDiffPruning(nn.Module):
             else:
                 return x, out_pred_prob
         else:
-            # file.close()
             return x, sparse
 
 class VisionTransformerTeacher(nn.Module):
@@ -642,3 +643,18 @@ def test_irregular_sparsity(name,matrix):
 
     return zeros,non_zeros
 
+
+
+def write_excel_xls_append(path, value):
+    index = len(value)  # 获取需要写入数据的行数
+    workbook = xlrd.open_workbook(path)  # 打开工作簿
+    sheets = workbook.sheet_names()  # 获取工作簿中的所有表格
+    worksheet = workbook.sheet_by_name(sheets[0])  # 获取工作簿中所有表格中的的第一个表格
+    rows_old = worksheet.nrows  # 获取表格中已存在的数据的行数
+    new_workbook = copy(workbook)  # 将xlrd对象拷贝转化为xlwt对象
+    new_worksheet = new_workbook.get_sheet(0)  # 获取转化后工作簿中的第一个表格
+    for i in range(0, index):
+        for j in range(0, len(value[i])):
+            new_worksheet.write(i+rows_old, j, value[i][j])  # 追加写入数据，注意是从i+rows_old行开始写入
+    new_workbook.save(path)  # 保存工作簿
+    print('xls sheet append done')
