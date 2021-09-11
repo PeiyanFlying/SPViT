b1a762c95cbf13886380ceb3c63908b172432e0b
ZKong <kong.zhe@husky.neu.edu>
2021-09-11 19:20:08 -0400

    prevent nan

--- main_l2_vit.py
+++ main_l2_vit.py
@@ -152,7 +152,7 @@ def get_args_parser():
                         help='path where to save, empty for no saving')
     parser.add_argument('--device', default='cuda',
                         help='device to use for training / testing')
-    parser.add_argument('--seed', default=42, type=int)
+    parser.add_argument('--seed', default=0, type=int)
     parser.add_argument('--resume', default='', help='resume from checkpoint')
     parser.add_argument('--start_epoch', default=0, type=int, metavar='N',
                         help='start epoch')
