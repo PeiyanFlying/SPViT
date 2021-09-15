54a58fc37f278d0a3321c32ddc92d76ab6cc34e0
ZKong <kong.zhe@husky.neu.edu>
2021-09-14 20:43:52 -0400

    amp false

--- engine_l2.py
+++ engine_l2.py
@@ -37,7 +37,7 @@ def train_one_epoch(model: torch.nn.Module, criterion: DistillationLoss,
 
         if mixup_fn is not None:
             samples, targets = mixup_fn(samples, targets)
-        with torch.cuda.amp.autocast():
+        with torch.cuda.amp.autocast(enabled=False):
             outputs = model(samples)
 
             loss = criterion(samples, outputs, targets)
@@ -93,7 +93,7 @@ def evaluate(data_loader, model, device):
         target = target.to(device, non_blocking=True)
 
         # compute output
-        with torch.cuda.amp.autocast():
+        with torch.cuda.amp.autocast(enabled=False):
             output_all = model(images)
             output, sparse = output_all
             loss = criterion(output, target)
