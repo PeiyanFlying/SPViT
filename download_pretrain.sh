06e3adb898b30de9ff81d5242b64169a0f5ecf86
PEIYAN <45463777+PeiyanFlying@users.noreply.github.com>
2021-09-22 16:34:51 -0400

    Add files via upload

--- download_pretrain.sh
+++ download_pretrain.sh
@@ -1,4 +1,8 @@
 wget https://dl.fbaipublicfiles.com/deit/deit_small_patch16_224-cd65a155.pth
+wget https://dl.fbaipublicfiles.com/deit/deit_base_patch16_224-b5f2ef4d.pth
 wget https://github.com/zihangJiang/TokenLabeling/releases/download/1.0/lvvit_s-26M-224-83.3.pth.tar
 wget https://github.com/zihangJiang/TokenLabeling/releases/download/1.0/lvvit_m-56M-224-84.0.pth.tar
 wget https://dl.fbaipublicfiles.com/deit/deit_tiny_patch16_224-a1311bcf.pth
+wget https://drive.google.com/file/d/1whd2saHjELyj6ESdkXSD1x8mMMYrRGNV/view?usp=sharing # pit_b
+wget https://drive.google.com/file/d/1shVYwAmYesWioca7NiDM2ESrZYEjZsaG/view?usp=sharing # pit_s
+wget https://drive.google.com/file/d/1F7x2owp7frFJoyojoF80L7yB-DA9jhmi/view?usp=sharing # pit_xs
